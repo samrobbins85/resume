2e47cb9a55cede5c2fe4e3ace7c4a1900440c4c2
Sam Robbins <samrobbinsgb@gmail.com>
2021-04-04 19:28:08 +0100

    ✨ Add initial design of skills section

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,49 @@ import Head from "next/head";
 import { Linkedin, GitHub, Globe, Mail } from "react-feather";
 import getData from "../lib/cms";
 
+function Social({ email, website, github, linkedin }) {
+	return (
+		<div className="flex justify-around">
+			<div className="flex">
+				<Mail className="mr-2" />
+				<a className="underline" href={`mailto:${email}`}>
+					{email}
+				</a>
+			</div>
+			<div className="flex">
+				<Globe className="mr-2" />
+				<a className="underline" href={`https://${website}`}>
+					{website}
+				</a>
+			</div>
+			<div className="flex">
+				<GitHub className="mr-2" />
+				<a className="underline" href={`https://github.com/${github}`}>
+					{github}
+				</a>
+			</div>
+			<div className="flex">
+				<Linkedin className="mr-2" />
+				<a
+					className="underline"
+					href={`https://www.linkedin.com/in/${linkedin}`}
+				>
+					{linkedin}
+				</a>
+			</div>
+		</div>
+	);
+}
+
+function SkillList({ title, list }) {
+	return (
+		<div className="grid grid-cols-3">
+			<div className="font-bold">{title}</div>
+			<div className="col-span-2">{list.join(", ")}</div>
+		</div>
+	);
+}
+
 export default function IndexPage({ data }) {
 	return (
 		<>
@@ -13,39 +56,25 @@ export default function IndexPage({ data }) {
 				<h1 className="text-center text-3xl font-medium py-4">
 					{data.name}
 				</h1>
-				<div className="flex justify-around">
-					<div className="flex">
-						<Mail className="mr-2" />
-						<a className="underline" href={`mailto:${data.email}`}>
-							{data.email}
-						</a>
-					</div>
-					<div className="flex">
-						<Globe className="mr-2" />
-						<a
-							className="underline"
-							href={`https://${data.website}`}
-						>
-							{data.website}
-						</a>
-					</div>
-					<div className="flex">
-						<GitHub className="mr-2" />
-						<a
-							className="underline"
-							href={`https://github.com/${data.github}`}
-						>
-							{data.github}
-						</a>
-					</div>
-					<div className="flex">
-						<Linkedin className="mr-2" />
-						<a
-							className="underline"
-							href={`https://www.linkedin.com/in/${data.linkedin}`}
-						>
-							{data.linkedin}
-						</a>
+				<Social
+					email={data.email}
+					website={data.website}
+					github={data.github}
+					linkedin={data.linkedin}
+				/>
+				<div className="grid grid-cols-4 py-4">
+					<div className="text-2xl">Skills</div>
+					<div className="col-span-3 grid gap-y-1">
+						<SkillList
+							title="Programming Languages"
+							list={data.languages}
+						/>
+						<SkillList
+							title="Libraries & Frameworks"
+							list={data.librariesAndFrameworks}
+						/>
+						<SkillList title="Software" list={data.software} />
+						<SkillList title="Platforms" list={data.platforms} />
 					</div>
 				</div>
 			</div>
