e760a0ba0c546c37bd01af518d9ad103abbf9a30
Sam Robbins <samrobbinsgb@gmail.com>
2021-04-04 18:55:51 +0100

    🎉 Start the project!

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,61 @@
 import Head from "next/head";
+import { Linkedin, GitHub, Globe, Mail } from "react-feather";
+import getData from "../lib/cms";
 
-export default function IndexPage() {
+export default function IndexPage({ data }) {
 	return (
 		<>
 			<Head>
-				<title>Next.js Template</title>
-				<meta
-					name="Description"
-					content="My template Next.js application"
-				/>
+				<title>Resume</title>
+				<meta name="Description" content="My resume" />
 			</Head>
-			<div>
-				<h1 className="text-center">Hello, World </h1>
+			<div className="max-w-85ch mx-auto">
+				<h1 className="text-center text-3xl font-medium py-4">
+					{data.name}
+				</h1>
+				<div className="flex justify-around">
+					<div className="flex">
+						<Mail className="mr-2" />
+						<a className="underline" href={`mailto:${data.email}`}>
+							{data.email}
+						</a>
+					</div>
+					<div className="flex">
+						<Globe className="mr-2" />
+						<a
+							className="underline"
+							href={`https://${data.website}`}
+						>
+							{data.website}
+						</a>
+					</div>
+					<div className="flex">
+						<GitHub className="mr-2" />
+						<a
+							className="underline"
+							href={`https://github.com/${data.github}`}
+						>
+							{data.github}
+						</a>
+					</div>
+					<div className="flex">
+						<Linkedin className="mr-2" />
+						<a
+							className="underline"
+							href={`https://www.linkedin.com/in/${data.linkedin}`}
+						>
+							{data.linkedin}
+						</a>
+					</div>
+				</div>
 			</div>
 		</>
 	);
+}
+
+export async function getStaticProps() {
+	const data = await getData();
+	return {
+		props: { data },
+	};
 }
